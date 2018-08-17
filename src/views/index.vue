0a9c39fe777696dcf05af1f1723d6d3afa7fba8b
左建新 <zuojianxin001@outlook.com>
2018-08-17 19:24:02 +0800

    列表展示

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,13 +1,25 @@
 <template>
   <mu-paper :z-depth="1">
-    <mu-data-table :columns="columns" :sort.sync="sort" @sort-change="handleSortChange" :data="list">
-      <!-- <template slot-scope="scope">
-        <td>{{scope.row.name}}</td>
-        <td class="is-right">{{scope.row.calories}}</td>
-        <td class="is-right">{{scope.row.fat}}</td>
-        <td class="is-right">{{scope.row.carbs}}</td>
-      </template> -->
-    </mu-data-table>
+    <mu-list textline="two-line">
+      <mu-list-item avatar :ripple="false" button>
+        <mu-list-item-content>
+          <mu-list-item-title>
+            资产：gdex.STB : bitCNY	
+          </mu-list-item-title>
+          <mu-list-item-sub-title style="color: rgba(0, 0, 0, .87)">
+            单价：0.06900
+          </mu-list-item-sub-title>
+          <mu-list-item-sub-title>
+            日涨跌：8.48%
+          </mu-list-item-sub-title>
+        </mu-list-item-content>
+        <mu-list-item-action >
+          <mu-list-item-after-text>15 min</mu-list-item-after-text>
+          <mu-checkbox color="yellow700" v-model="selects" value="value1" uncheck-icon="star_border" checked-icon="star"></mu-checkbox>
+        </mu-list-item-action>
+    </mu-list-item>
+    <mu-divider></mu-divider>
+    </mu-list>
   </mu-paper>
 </template>
 <script>
@@ -15,6 +27,7 @@ export default {
   name: 'index',
   data () {
     return {
+      selects: 0,
       sort: {
         name: '',
         order: 'asc'
